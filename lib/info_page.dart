55e849c0bb1f4cb62065a52c07853127900b62eb
ZiyanK <ziyankarmali786@gmail.com>
2020-01-03 17:00:33 +0530

    finished info page

--- lib/info_page.dart
+++ lib/info_page.dart
@@ -11,20 +11,77 @@ class InfoPage extends StatefulWidget {
 }
 
 class _InfoPageState extends State<InfoPage> {
+  listItem(String img, String place, String price) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Stack(
+        children: <Widget>[
+          Container(
+            height: 200.0,
+            width: 150.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(7.0),
+              image: DecorationImage(
+                  image: AssetImage(img),
+                  fit: BoxFit.cover,
+                  colorFilter: ColorFilter.mode(
+                      Colors.black.withOpacity(0.6), BlendMode.darken)),
+            ),
+          ),
+          Positioned(
+            top: 15.0,
+            right: 15.0,
+            child: Container(
+              height: 30.0,
+              width: 30.0,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5.0),
+                  color: Colors.white),
+              child: Center(
+                child: Icon(
+                  Icons.bookmark_border,
+                  color: Colors.pink.shade400,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 120.0,
+            left: 15.0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  place,
+                  style: GoogleFonts.montserrat(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 15.0,
+                      textStyle: TextStyle(color: Colors.white)),
+                ),
+                SizedBox(height: 5.0),
+                Text(
+                  price,
+                  style: GoogleFonts.montserrat(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 14.0,
+                    textStyle: TextStyle(color: Colors.white)),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: <Widget>[
           Container(
-            height: MediaQuery
-                .of(context)
-                .size
-                .height,
-            width: MediaQuery
-                .of(context)
-                .size
-                .width,
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
             decoration: BoxDecoration(
               image: DecorationImage(
                 image: AssetImage(widget.img),
@@ -39,7 +96,7 @@ class _InfoPageState extends State<InfoPage> {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.only(top: 35.0, right: 15.0),
+            padding: const EdgeInsets.only(top: 40.0, right: 15.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
@@ -79,6 +136,148 @@ class _InfoPageState extends State<InfoPage> {
                   ),
                 ),
               ],
+            ),
+          ),
+          Positioned(
+            top: 100.0,
+            child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    width: MediaQuery.of(context).size.width - 10.0,
+                    child: Padding(
+                      padding: EdgeInsets.only(right: 15.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            'Trending Attractions',
+                            style: GoogleFonts.montserrat(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25.0,
+                              textStyle: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          Icon(
+                            Icons.more_vert,
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 12.0),
+                    child: Stack(
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: Container(
+                            height: 220.0,
+                            width: 370.0,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10.0),
+                              image: DecorationImage(
+                                image: AssetImage('images/kyoto.jpg'),
+                                fit: BoxFit.cover,
+                                colorFilter: ColorFilter.mode(
+                                    Colors.black.withOpacity(0.6),
+                                    BlendMode.darken),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: 150.0,
+                          left: 30.0,
+                          child: Container(
+                            width: MediaQuery.of(context).size.width - 60.0,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      'Kyoto Tour',
+                                      style: GoogleFonts.montserrat(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20.0,
+                                          textStyle: TextStyle(
+                                            color: Colors.white,
+                                          )),
+                                    ),
+                                    SizedBox(
+                                      height: 5.0,
+                                    ),
+                                    Text(
+                                      'Three day tour around Kyoto',
+                                      style: GoogleFonts.montserrat(
+                                          fontSize: 14.0,
+                                          textStyle:
+                                              TextStyle(color: Colors.white)),
+                                    ),
+                                  ],
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(right: 20.0),
+                                  child: Container(
+                                    height: 30.0,
+                                    width: 30.0,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(5.0),
+                                      color: Colors.white,
+                                    ),
+                                    child: Icon(
+                                      Icons.arrow_forward_ios,
+                                      color: Colors.pink.shade400,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 15.0),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: Text(
+                      'Weekly Highlights',
+                      style: GoogleFonts.montserrat(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25.0,
+                        textStyle: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15.0),
+                  Container(
+                    height: 200.0,
+                    width: MediaQuery.of(context).size.width,
+                    child: ListView(
+                      padding: EdgeInsets.only(right: 15.0),
+                      scrollDirection: Axis.horizontal,
+                      children: <Widget>[
+                        listItem('images/takashi.jpg', 'Takashi Castle',
+                            '\$200-\$400'),
+                        listItem('images/heaven.jpeg', 'Heaven\'s Gate',
+                            '\$50-\$150'),
+                        listItem('images/niagra.jpg', 'Niagra Falls',
+                            '\$300-\$400'),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ],
